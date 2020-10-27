c4ccea7ac279bb5e2e6a40b12241c164f9f37aaf
TiiaJusslin <tiia.jusslin@gmail.com>
2020-10-27 10:41:14 +0200

    Deleting todos

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const todoList = document.querySelector(".todo-list");
 
 //Event listeners
 todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteCheck);
 
 //Functions
 function addTodo(event){
@@ -13,10 +14,10 @@ function addTodo(event){
     const todoDiv = document.createElement("div");
     todoDiv.classList.add("todo");
     const newToDo = document.createElement("li"); 
-    newToDo.innerText = "hey";
+    newToDo.innerText = todoInput.value;
     newToDo.classList.add("todo-item");
     todoDiv.appendChild(newToDo);
-    //chekc mark button
+    //check mark button
     const completedButton = document.createElement("button");
     completedButton.innerHTML = '<i class ="fas fa-check"></i>';
     completedButton.classList.add("complete-btn");
@@ -28,4 +29,25 @@ function addTodo(event){
     todoDiv.appendChild(trashButton);
     //Append to list
     todoList.appendChild(todoDiv);
+    //Clear todo input value
+    todoInput.value =""; 
+}
+
+function deleteCheck(e) {
+    const item = e.target;
+    //Delete todo
+    if(item.classList[0] === "trash-btn") {
+       const todo = item.parentElement;
+       //Animation
+       todo.classList.add("fall");
+       todo.addEventListener("transitioned", function(){
+        todo.remove();
+       });
+    }
+
+    //Chekc mark
+    if(item.classList[0] === "complete-btn") {
+        const todo = item.parentElement;
+        todo.classList.toggle("completed");
+    }
 }
