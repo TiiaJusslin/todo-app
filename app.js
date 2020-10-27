372b2e6ffd2a56e150e165b5c2860f2e910e115d
TiiaJusslin <tiia.jusslin@gmail.com>
2020-10-27 13:20:56 +0200

    Adds filter option

--- app.js
+++ app.js
@@ -2,10 +2,12 @@
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
+const filterOption = document.querySelector(".filter-todo");
 
 //Event listeners
 todoButton.addEventListener("click", addTodo);
 todoList.addEventListener("click", deleteCheck);
+filterOption.addEventListener("click", filterTodo);
 
 //Functions
 function addTodo(event){
@@ -45,9 +47,31 @@ function deleteCheck(e) {
        });
     }
 
-    //Chekc mark
+    //Check mark
     if(item.classList[0] === "complete-btn") {
         const todo = item.parentElement;
         todo.classList.toggle("completed");
     }
+}
+
+function filterTodo(e) {
+    const todos = todoList.childNodes;
+    todos.forEach(function(todo){
+        switch(e.target.value){
+            case "all":
+                todo.style.display = "flex";
+            case "completed":
+                if (todo.classList.contains("completed")){
+                    todo.style.display = "flex";
+                }else{
+                    todo.style.display = "none";
+                }
+            case "uncompleted":
+                if (!todo.classList.contains("completed")){
+                    todo.style.display = "flex";
+                }else{
+                    todo.style.display = "none";
+                }
+        }
+    });
 }
